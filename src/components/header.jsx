f2593a9ee88b194ea51bd54bfa3c9b86a81d0a17
harshgupta80700 <harshgupta.80700@gmail.com>
2021-07-01 18:01:15 +0530

    deafult props and prop types

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div>
+            {props.title}
             Header Component!!
+            {console.log("this is from console log")}
         </div>
     )
+}
+
+Header.defaultProps = {
+    title: "yo"
+}
+
+Header.prototype = {
+    title: PropTypes.string
 }
