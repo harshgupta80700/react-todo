3aa4a13bc888978da9340da57290d6ae0ba8fd00
harshgupta80700 <harshgupta.80700@gmail.com>
2021-07-01 17:41:39 +0530

    todos and todo card component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Header from "./components/header";
+import Header from "./components/header"
 import Footer from "./components/footer"
+import Todos from './components/todos/todos';
 
 function App() {
   return (
     <>
     <Header/>
+    <Todos/>
     <Footer/>
     </>
   );
---
+++ src/components/todos/todos.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TodoCard from './todo-card'
+
+
+export default function Todos() {
+    return (
+        <div>
+            This will contains todos!!
+            <TodoCard/>
+            <TodoCard/>
+            <TodoCard/>
+            <TodoCard/>
+        </div>
+    )
+}
